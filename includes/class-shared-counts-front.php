3a92e1399550c9518a9a03a39cc56224b54c8928
Jared Atchison <jared@jaredatchison.com>
2018-01-22 21:50:58 -0600

    Improve i18n for share buttons

--- includes/class-shared-counts-front.php
+++ includes/class-shared-counts-front.php
@@ -461,59 +461,59 @@ class Shared_Counts_Front {
 			switch ( $type ) {
 				case 'facebook':
 					$link['link']       = 'https://www.facebook.com/sharer/sharer.php?u=' . $link['url'] . '&display=popup&ref=plugin&src=share_button';
-					$link['label']      = 'Facebook';
+					$link['label']      = esc_html__( 'Facebook', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18.8125" height="32" viewBox="0 0 602 1024"><path d="M548 6.857v150.857h-89.714q-49.143 0-66.286 20.571t-17.143 61.714v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571q0-106.286 59.429-164.857t158.286-58.571q84 0 130.286 6.857z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on Facebook';
+					$link['attr_title'] = esc_html__( 'Share on Facebook', 'shared-counts' );
 					break;
 				case 'facebook_likes':
 					$link['link']       = 'https://www.facebook.com/plugins/like.php?href=' . $link['url'];
-					$link['label']      = 'Like';
+					$link['label']      = esc_html__( 'Like', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18.8125" height="32" viewBox="0 0 602 1024"><path d="M548 6.857v150.857h-89.714q-49.143 0-66.286 20.571t-17.143 61.714v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571q0-106.286 59.429-164.857t158.286-58.571q84 0 130.286 6.857z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Like on Facebook';
+					$link['attr_title'] = esc_html__( 'Like on Facebook', 'shared-counts' );
 					break;
 				case 'facebook_shares':
 					$link['link']       = 'https://www.facebook.com/sharer/sharer.php?u=' . $link['url'] . '&display=popup&ref=plugin&src=share_button';
-					$link['label']      = 'Share';
+					$link['label']      = esc_html__( 'Share', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18.8125" height="32" viewBox="0 0 602 1024"><path d="M548 6.857v150.857h-89.714q-49.143 0-66.286 20.571t-17.143 61.714v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571q0-106.286 59.429-164.857t158.286-58.571q84 0 130.286 6.857z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on Facebook';
+					$link['attr_title'] = esc_html__( 'Share on Facebook', 'shared-counts' );
 					break;
 				case 'twitter':
 					$link['link']       = 'https://twitter.com/share?url=' . $link['url'] . '&text=' . rawurlencode( $link['title'] );
-					$link['label']      = 'Tweet';
+					$link['label']      = esc_html__( 'Tweet', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="29.71875" height="32" viewBox="0 0 951 1024"><path d="M925.714 233.143q-38.286 56-92.571 95.429 0.571 8 0.571 24 0 74.286-21.714 148.286t-66 142-105.429 120.286-147.429 83.429-184.571 31.143q-154.857 0-283.429-82.857 20 2.286 44.571 2.286 128.571 0 229.143-78.857-60-1.143-107.429-36.857t-65.143-91.143q18.857 2.857 34.857 2.857 24.571 0 48.571-6.286-64-13.143-106-63.714t-42-117.429v-2.286q38.857 21.714 83.429 23.429-37.714-25.143-60-65.714t-22.286-88q0-50.286 25.143-93.143 69.143 85.143 168.286 136.286t212.286 56.857q-4.571-21.714-4.571-42.286 0-76.571 54-130.571t130.571-54q80 0 134.857 58.286 62.286-12 117.143-44.571-21.143 65.714-81.143 101.714 53.143-5.714 106.286-28.571z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on Twitter';
+					$link['attr_title'] = esc_html__( 'Share on Twitter', 'shared-counts' );
 					break;
 				case 'pinterest':
 					$link['link']       = 'https://pinterest.com/pin/create/button/?url=' . $link['url'] . '&media=' . $link['img'] . '&description=' . $link['title'];
-					$link['label']      = 'Pin';
+					$link['label']      = esc_html__( 'Pin', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="22.84375" height="32" viewBox="0 0 731 1024"><path d="M0 341.143q0-61.714 21.429-116.286t59.143-95.143 86.857-70.286 105.714-44.571 115.429-14.857q90.286 0 168 38t126.286 110.571 48.571 164q0 54.857-10.857 107.429t-34.286 101.143-57.143 85.429-82.857 58.857-108 22q-38.857 0-77.143-18.286t-54.857-50.286q-5.714 22.286-16 64.286t-13.429 54.286-11.714 40.571-14.857 40.571-18.286 35.714-26.286 44.286-35.429 49.429l-8 2.857-5.143-5.714q-8.571-89.714-8.571-107.429 0-52.571 12.286-118t38-164.286 29.714-116q-18.286-37.143-18.286-96.571 0-47.429 29.714-89.143t75.429-41.714q34.857 0 54.286 23.143t19.429 58.571q0 37.714-25.143 109.143t-25.143 106.857q0 36 25.714 59.714t62.286 23.714q31.429 0 58.286-14.286t44.857-38.857 32-54.286 21.714-63.143 11.429-63.429 3.714-56.857q0-98.857-62.571-154t-163.143-55.143q-114.286 0-190.857 74t-76.571 187.714q0 25.143 7.143 48.571t15.429 37.143 15.429 26 7.143 17.429q0 16-8.571 41.714t-21.143 25.714q-1.143 0-9.714-1.714-29.143-8.571-51.714-32t-34.857-54-18.571-61.714-6.286-60.857z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on Pinterest';
+					$link['attr_title'] = esc_html__( 'Share on Pinterest', 'shared-counts' );
 					break;
 				case 'linkedin':
 					$link['link']       = 'https://www.linkedin.com/shareArticle?mini=true&url=' . $link['url'];
-					$link['label']      = 'LinkedIn';
+					$link['label']      = esc_html__( 'LinkedIn', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="27.4375" height="32" viewBox="0 0 878 1024"><path d="M199.429 357.143v566.286h-188.571v-566.286h188.571zM211.429 182.286q0.571 41.714-28.857 69.714t-77.429 28h-1.143q-46.857 0-75.429-28t-28.571-69.714q0-42.286 29.429-70t76.857-27.714 76 27.714 29.143 70zM877.714 598.857v324.571h-188v-302.857q0-60-23.143-94t-72.286-34q-36 0-60.286 19.714t-36.286 48.857q-6.286 17.143-6.286 46.286v316h-188q1.143-228 1.143-369.714t-0.571-169.143l-0.571-27.429h188v82.286h-1.143q11.429-18.286 23.429-32t32.286-29.714 49.714-24.857 65.429-8.857q97.714 0 157.143 64.857t59.429 190z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on LinkedIn';
+					$link['attr_title'] = esc_html__( 'Share on LinkedIn', 'shared-counts' );
 					break;
 				case 'google':
 					$link['link']       = 'https://plus.google.com/share?url=' . $link['url'];
-					$link['label']      = 'Google+';
+					$link['label']      = esc_html__( 'Google+', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="24.880789674" viewBox="0 0 1317 1024"><path d="M821.143 521.714q0 118.857-49.714 211.714t-141.714 145.143-210.857 52.286q-85.143 0-162.857-33.143t-133.714-89.143-89.143-133.714-33.143-162.857 33.143-162.857 89.143-133.714 133.714-89.143 162.857-33.143q163.429 0 280.571 109.714l-113.714 109.143q-66.857-64.571-166.857-64.571-70.286 0-130 35.429t-94.571 96.286-34.857 132.857 34.857 132.857 94.571 96.286 130 35.429q47.429 0 87.143-13.143t65.429-32.857 44.857-44.857 28-47.429 12.286-42.286h-237.714v-144h395.429q6.857 36 6.857 69.714zM1316.571 452v120h-119.429v119.429h-120v-119.429h-119.429v-120h119.429v-119.429h120v119.429h119.429z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on Google+';
+					$link['attr_title'] = esc_html__( 'Share on Google+', 'shared-counts' );
 					break;
 				case 'stumbleupon':
 					$link['link']       = 'https://www.stumbleupon.com/submit?url=' . $link['url'] . '&title=' . $link['title'];
-					$link['label']      = 'StumbleUpon';
+					$link['label']      = esc_html__( 'StumbleUpon', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="29.870556062" viewBox="0 0 1097 1024"><path d="M606.857 406.857v-67.429q0-24-17.143-41.143t-41.143-17.143-41.143 17.143-17.143 41.143v349.714q0 100-72 170.857t-173.143 70.857q-101.714 0-173.429-71.714t-71.714-173.429v-152h187.429v149.714q0 24.571 17.143 41.429t41.143 16.857 41.143-16.857 17.143-41.429v-354.286q0-97.714 72.286-166.857t172.286-69.143q100.571 0 172.571 69.714t72 168v77.714l-111.429 33.143zM909.714 533.714h187.429v152q0 101.714-71.714 173.429t-173.429 71.714q-101.143 0-173.143-71.143t-72-171.714v-153.143l74.857 34.857 111.429-33.143v154.286q0 24 17.143 40.857t41.143 16.857 41.143-16.857 17.143-40.857v-157.143z"></path></svg>';
 					$link['target']     = '_blank';
-					$link['attr_title'] = 'Share on StumbleUpon';
+					$link['attr_title'] = esc_html__( 'Share on StumbleUpon', 'shared-counts' );
 					break;
 				case 'included_total':
 					$link['link']   = '';
@@ -523,13 +523,13 @@ class Shared_Counts_Front {
 					break;
 				case 'print':
 					$link['link']       = 'javascript:window.print()';
-					$link['label']      = 'Print';
+					$link['label']      = esc_html__( 'Print', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="29.71875" height="32" viewBox="0 0 951 1024"><path d="M219.429 877.714h512v-146.286h-512v146.286zM219.429 512h512v-219.429h-91.429q-22.857 0-38.857-16t-16-38.857v-91.429h-365.714v365.714zM877.714 548.571q0-14.857-10.857-25.714t-25.714-10.857-25.714 10.857-10.857 25.714 10.857 25.714 25.714 10.857 25.714-10.857 10.857-25.714zM950.857 548.571v237.714q0 7.429-5.429 12.857t-12.857 5.429h-128v91.429q0 22.857-16 38.857t-38.857 16h-548.571q-22.857 0-38.857-16t-16-38.857v-91.429h-128q-7.429 0-12.857-5.429t-5.429-12.857v-237.714q0-45.143 32.286-77.429t77.429-32.286h36.571v-310.857q0-22.857 16-38.857t38.857-16h384q22.857 0 50.286 11.429t43.429 27.429l86.857 86.857q16 16 27.429 43.429t11.429 50.286v146.286h36.571q45.143 0 77.429 32.286t32.286 77.429z"></path></svg>';
-					$link['attr_title'] = 'Print this Page';
+					$link['attr_title'] = esc_html__( 'Print this Page', 'shared-counts' );
 					break;
 				case 'email':
 					$link['link']       = '#shared-counts-email';
-					$link['label']      = 'Email';
+					$link['label']      = esc_html__( 'Email', 'shared-counts' );
 					$link['icon']       = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 1024 1024"><path d="M1024 405.714v453.714q0 37.714-26.857 64.571t-64.571 26.857h-841.143q-37.714 0-64.571-26.857t-26.857-64.571v-453.714q25.143 28 57.714 49.714 206.857 140.571 284 197.143 32.571 24 52.857 37.429t54 27.429 62.857 14h1.143q29.143 0 62.857-14t54-27.429 52.857-37.429q97.143-70.286 284.571-197.143 32.571-22.286 57.143-49.714zM1024 237.714q0 45.143-28 86.286t-69.714 70.286q-214.857 149.143-267.429 185.714-5.714 4-24.286 17.429t-30.857 21.714-29.714 18.571-32.857 15.429-28.571 5.143h-1.143q-13.143 0-28.571-5.143t-32.857-15.429-29.714-18.571-30.857-21.714-24.286-17.429q-52-36.571-149.714-104.286t-117.143-81.429q-35.429-24-66.857-66t-31.429-78q0-44.571 23.714-74.286t67.714-29.714h841.143q37.143 0 64.286 26.857t27.143 64.571z"></path></svg>';
 					$link['target']     = '';
 					$link['attr_title'] = 'Share via Email';
