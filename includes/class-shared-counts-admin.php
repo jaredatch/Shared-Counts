4a174939a1449438290b0db419283374af44c0b5
Bill Erickson <bill.erickson@gmail.com>
2018-03-21 11:40:38 -0500

    in admin column, use post meta, prevents triggering an update

--- includes/class-shared-counts-admin.php
+++ includes/class-shared-counts-admin.php
@@ -775,7 +775,7 @@ class Shared_Counts_Admin {
 	 */
 	public function shared_count_column( $column, $post_id ) {
 		if( 'shared_counts' == $column )
-			shared_counts()->core->count( $post_id, 'total', $echo = true, $round = 2 );
+			echo get_post_meta( $post_id, 'shared_counts_total', true );
 	}
 
 	/**
